8244be7da26668f0b30bb3a0549effc617d8dd98
MATEENNANDGADI <mateennandgadi@gmail.com>
2025-08-19 22:43:53 +0530

    array: array1

--- array.c
+++ array.c
@@ -4,11 +4,20 @@ struct myArray
 {
     int totalsize;
     int usedsize;
+    int *ptr;
 
 };
 
+void create(struct myArray * b, int t , int u,){
+    (*b).totalsize = t;
+    (*b).usedsize = u;
+    int * ptr = ((int *) malloc(t* sizeof(int)) ) 
+}
+
+
 
 int main() { 
+    struct myArray marks;
     
     return 0;
 } 
