96ee3e96e25b129952af286c15214744bb06cac6
MATEENNANDGADI <mateennandgadi@gmail.com>
2025-08-20 18:39:57 +0530

    insertion.: insertion with display

--- insertion.c
+++ insertion.c
@@ -1,6 +1,22 @@
 #include <stdio.h>
 
-int main() { 
+void t(int arr[] , int n) {
+    for (int i = 0; i < n; i++ ){
+        printf("%d\n", arr[i]);
+    }
+};
+
+void insert(int arr) {
     
+}
+
+
+int main() { 
+    int arr[100] = {1,2,6,88};
+    printf("%d\n",arr[2]);   
+    t(arr , 4);
+    insertion(arr);
+
+  
     return 0;
 }
